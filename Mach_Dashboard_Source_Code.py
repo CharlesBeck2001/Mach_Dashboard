abb0f3c424a0df79c8d0c5bb8a21571a68004d52
CharlesBeck2001 <58281703+CharlesBeck2001@users.noreply.github.com>
2024-12-19 19:03:30 -0700

    Update Mach_Dashboard_Source_Code.py

--- Mach_Dashboard_Source_Code.py
+++ Mach_Dashboard_Source_Code.py
@@ -1704,6 +1704,7 @@ elif page == "Cumulative Volume Curves":
 
     # Fetch all unique pairs and display them
     df_pairs = execute_sql(sql_query_pairs)
+    df_pairs = pd.json_normalize(df_pairs['result'])
     if df_pairs is not None:
         st.write("Columns in df_pairs:", df_pairs.columns) 
         if 'source_chain' in df_pairs.columns and 'dest_chain' in df_pairs.columns:
@@ -1721,7 +1722,7 @@ elif page == "Cumulative Volume Curves":
 
             # Plot the "Total" curve
             if "Total" in selected_pairs:
-                sql_query_total = get_cvf_for_pair('', '')
+                sql_query_total = get_cvf_for_total()
                 df_cumulative_volume = execute_sql(sql_query_total)
                 if df_cumulative_volume is not None:
                     df_cumulative_volume['log_total_volume'] = np.log10(df_cumulative_volume['total_volume'])

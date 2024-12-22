1d128fb9c74e01c26ba244e7eb1f05067621e3c8
CharlesBeck2001 <58281703+CharlesBeck2001@users.noreply.github.com>
2024-12-21 21:16:36 -0700

    Update Mach_Dashboard_Source_Code.py

--- Mach_Dashboard_Source_Code.py
+++ Mach_Dashboard_Source_Code.py
@@ -1731,6 +1731,7 @@ elif page == "Cumulative Volume Curves":
                     #st.write("Columns in df_cumulative_volume (Total):", df_cumulative_volume.columns)
                     df_cumulative_volume['pair'] = 'Total'
                     df_cumulative_volume = df_cumulative_volume[df_cumulative_volume['log_total_volume'] >= 0]
+                    st.write("Tota", df_cumulative_volume)
                     #df_filtered = df_cumulative_volume[df_cumulative_volume['log_total_volume'] >= 0]
                     plot_data_list.append(df_cumulative_volume[['log_total_volume', 'cumulative_percentage', 'pair']])
 

55c1b8eec17f93d64103e9db4640bd8247fc34e9
CharlesBeck2001 <58281703+CharlesBeck2001@users.noreply.github.com>
2024-12-21 20:36:55 -0700

    Update Mach_Dashboard_Source_Code.py

--- Mach_Dashboard_Source_Code.py
+++ Mach_Dashboard_Source_Code.py
@@ -1750,9 +1750,9 @@ elif page == "Cumulative Volume Curves":
             if plot_data_list:
                 # Avoid averaging cumulative_percentage across pairs
                 combined_plot_data = pd.concat(plot_data_list,  ignore_index=True)
-
+                st.write("combined_plot_data:", combined_plot_data)
                 # Ensure each pair's cumulative_percentage is calculated independently
-                combined_plot_data = combined_plot_data.pivot_table(index='log_total_volume', columns='pair', values='cumulative_percentage', aggfunc='mean')
+                #combined_plot_data = combined_plot_data.pivot_table(index='log_total_volume', columns='pair', values='cumulative_percentage', aggfunc='mean')
                 # Plot all the curves
                 st.line_chart(combined_plot_data)
         else:

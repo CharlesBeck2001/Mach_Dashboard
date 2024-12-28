008ffb3b64417f0d7dbd11c0864127413d97ac4d
CharlesBeck2001 <58281703+CharlesBeck2001@users.noreply.github.com>
2024-12-28 01:02:14 -0700

    Update Mach_Dashboard_Source_Code.py

--- Mach_Dashboard_Source_Code.py
+++ Mach_Dashboard_Source_Code.py
@@ -1725,13 +1725,14 @@ elif page == "Cumulative Volume Curves":
                 sql_query_total = get_cvf_for_total()
                 df_cumulative_volume = execute_sql(sql_query_total)
                 df_cumulative_volume = pd.json_normalize(df_cumulative_volume['result'])
-                st.write(df_cumulative_volume)
                 if df_cumulative_volume is not None:
                     #st.write("Columns in df_cumulative_volume (Total):", df_cumulative_volume.columns)
                     df_cumulative_volume['log_total_volume'] = np.log10(df_cumulative_volume['total_volume'])
                     #st.write("Columns in df_cumulative_volume (Total):", df_cumulative_volume.columns)
                     df_cumulative_volume['pair'] = 'Total'
+                    st.write(df_cumulative_volume)
                     df_cumulative_volume = df_cumulative_volume[df_cumulative_volume['log_total_volume'] >= 0]
+                        
                     #st.write("Tota", df_cumulative_volume)
                     #df_filtered = df_cumulative_volume[df_cumulative_volume['log_total_volume'] >= 0]
                     plot_data_list.append(df_cumulative_volume[['log_total_volume', 'cumulative_percentage', 'pair']])
